2b0e5a9aeae6809c3cb1a3c724c5f96f30855d4b
guiguat <guaturadzn@gmail.com>
2020-06-19 19:47:03 -0300

    run index.html

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile('./build/index.html')
 
   // Open the DevTools.
   win.webContents.openDevTools()
