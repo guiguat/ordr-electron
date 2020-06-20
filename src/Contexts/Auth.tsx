65b727d01455b99a384a0006fe4c3dce952d5f16
guiguat <guaturadzn@gmail.com>
2020-06-20 13:54:08 -0300

    error changeState is not a function

--- src/Contexts/Auth.tsx
+++ src/Contexts/Auth.tsx
@@ -1,23 +1,63 @@
 import React, {useState, useEffect} from 'react';
-import firebase from 'firebase';
+import { User } from '@firebase/auth-types';
 import app from "../firebase";
 
 interface IAuthContext {
-    currentUser: firebase.User | null
+    currentUser?: User | null;
+    changeState():Promise<void>;
 }
 
-export const AuthContext = React.createContext<IAuthContext>({currentUser:null});
+export const AuthContext = React.createContext<IAuthContext>({} as IAuthContext);
 
 export const AuthProvider: React.FC = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState<firebase.User | null >(null);
+    const [currentUser, setCurrentUser] = useState<User| null>(null);
 
     useEffect(()=>{
-        app.auth().onAuthStateChanged(setCurrentUser);
+        app.auth().onAuthStateChanged(userState => setCurrentUser(userState));        
     },[]);
 
+    async function changeState(){
+        await app.auth().onAuthStateChanged(userState => setCurrentUser(userState));
+    }
+
+    
+
     return (
-        <AuthContext.Provider value={ {currentUser} }>
+        <AuthContext.Provider value={ { currentUser, changeState } }>
             {children}
         </AuthContext.Provider>
     );
 }
+
+// import React, { useEffect, useState } from "react";
+// import firebase from "../firebase";
+// type ContextProps = {
+//     currentUser: firebase.User | null;
+//     authenticated: boolean;
+//     setCurrentUser: any;
+//     loadingAuthState: boolean;
+// };
+// export const AuthContext = React.createContext<Partial<ContextProps>>({});
+// export const AuthProvider = ({ children }: any) => {
+
+//     const [currentUser, setCurrentUser] = useState(null as firebase.User | null);
+//     const [loadingAuthState, setLoadingAuthState] = useState(true);
+//     useEffect(() => {
+//         firebase.auth().onAuthStateChanged((user: any) => {
+//         setCurrentUser(user);
+//         setLoadingAuthState(false);
+//     });
+//     }, []);
+
+//     return (
+//         <AuthContext.Provider
+//         value={{
+//             currentUser,
+//             authenticated: currentUser !== null,
+//             setCurrentUser,
+//             loadingAuthState
+//         }}>
+//         {children} 
+//     </AuthContext.Provider>
+//     );
+// }
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,9 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/Auth';
+import { useHistory, Redirect } from 'react-router';
+import app from '../../firebase';
 
 // import './styles';
 
 const Login: React.FC = () => {
-  return <h1>Login</h1>;
+
+  const {currentUser, changeState} = useContext(AuthContext);
+
+  const history = useHistory();
+
+  async function handleLogIn (event:any){
+
+    event.preventDefault(); 
+
+    const { email, password } = event.target.elements;
+
+    try {
+      
+      await app
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value)
+        .then(res=>{
+          console.log(res);
+        })
+      changeState();
+      alert(currentUser);
+      history.push("/");
+    } catch (error) {
+
+      alert(error);
+
+    }
+
+    
+  };
+
+  if(!!authContext.currentUser){
+    return <Redirect to="/" />
+  }
+
+  return (
+    <>
+      <form onSubmit={handleLogIn}>
+        <label>
+          Email:
+          <input type="email" name="email" required/>
+        </label>
+        <br/>
+        <label>
+          Password:
+          <input type="password" name="password" required/>
+        </label>
+        <button type="submit">LogIn</button>
+      </form>
+    </>
+  );
 }
 
 export default Login;
