10786e1ee2308441b02bc1758c9a1c6a888915d1
guiguat <guaturadzn@gmail.com>
2020-06-20 20:04:53 -0300

    add navbar

---
+++ src/Components/NavBar/index.tsx
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../../Contexts/Auth';
+
+import './NavBar.scss';
+
+const NavBar: React.FC = () => {
+    const {LogOut} = useContext(AuthContext);
+    return (
+    
+        <nav className="navbar navbar-dark px-5 row bg-primary">
+            <span className="navbar-brand logo-lg mb-0 h1">ORDR.</span>
+            <ul className="navbar-nav row mr-auto mt-2 mt-lg-0">
+                <li className="nav-item ml-5">
+                    <a className="nav-link" href="/">Home</a>
+                </li>
+                <li className="nav-item ml-3">
+                    <a className="nav-link" href="/login">Link</a>
+                </li>
+            </ul>
+
+            <button className="btn text-white my-2 my-sm-0" onClick={LogOut}>LogOut</button>
+        </nav>
+  
+    );
+}
+
+export default NavBar;
