5e8ca08ca6327a70ad0a0abe3f3529f6e361c657
guiguat <guaturadzn@gmail.com>
2020-06-20 20:05:18 -0300

    add navbar

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import app from '../../firebase';
-
-// import './styles';
+import Navbar from "../../Components/NavBar";
 
 const Home: React.FC = () => {
   return (
     <>
+      <Navbar/>
       <h1>Home</h1>
       <button onClick={()=> app.auth().signOut()}>Sign Out</button>
     </>  
