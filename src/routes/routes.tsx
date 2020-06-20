7460f4465c1b21ca2ce2c75f5a84fc8b2026ef87
guiguat <guaturadzn@gmail.com>
2020-06-20 20:05:35 -0300

    update routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -9,26 +9,21 @@ import Home from '../Pages/Home';
 import "../custom.scss";
 
 const Routes: React.FC = () =>{
-    const {currentUser} = useContext(AuthContext);
-    console.log(currentUser);
-    return (
-        <>
-            <nav className="navbar navbar-dark bg-dark">
-                <span className="navbar-brand mb-0 h1">Navbar</span>
-            </nav>
-            <Router>
-                <Switch>
-                    <PrivateRoute 
-                    isAuth={!!currentUser} 
-                    path="/" 
-                    redirectPath="/login"
-                    exact 
-                    component={Home}
-                    />
-                    <Route path="/login" component={Login}/>
-                </Switch>
-            </Router>
-        </>
+    const { currentUser } = useContext(AuthContext);
+
+    return ( 
+        <Router>
+            <Switch>
+                <PrivateRoute 
+                isAuth={!!currentUser} 
+                path="/" 
+                redirectPath="/login"
+                exact 
+                component={Home}
+                />
+                <Route path="/login" component={Login}/>
+            </Switch>
+        </Router>
     );
 }
 
