2d524afa2e539ca7cc5d21adc6a883374c5cf0e4
guiguat <guaturadzn@gmail.com>
2020-06-20 20:26:57 -0300

    add SignUp function

--- src/Contexts/Auth.tsx
+++ src/Contexts/Auth.tsx
@@ -5,6 +5,12 @@ import app from "../firebase";
 interface IAuthContext {
     currentUser?: User | null;
     LogOut():void;
+    SignUp(signUpData: ISignUpData):void
+}
+interface ISignUpData{
+    email:string;
+    password:string;
+    name:string;
 }
 
 export const AuthContext = React.createContext<IAuthContext>({} as IAuthContext);
@@ -20,8 +26,19 @@ export const AuthProvider: React.FC = ({ children }) => {
         app.auth().signOut();
     }
 
+    function SignUp(signUpData:ISignUpData){
+        app.auth().createUserWithEmailAndPassword(signUpData.email, signUpData.password)
+            .then(result => {
+            return result.user?.updateProfile({
+                    displayName: signUpData.name
+                });
+            }).catch(function(error) {
+            console.log(error);
+            });
+    }
+
     return (
-        <AuthContext.Provider value={ { currentUser, LogOut } }>
+        <AuthContext.Provider value={ { currentUser, LogOut, SignUp } }>
             {children}
         </AuthContext.Provider>
     );
