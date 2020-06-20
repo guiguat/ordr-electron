357c2e20915d025658a635784b86bdc19db8a1da
guiguat <guaturadzn@gmail.com>
2020-06-20 10:05:17 -0300

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
+import Routes from './routes';
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <Routes/>
 }
 
 export default App;
