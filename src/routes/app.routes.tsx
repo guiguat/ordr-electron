10cbad78d8bae8aad4641d680d39a24ac9cb6f24
guiguat <guaturadzn@gmail.com>
2020-06-20 13:53:01 -0300

    change route path

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -5,7 +5,7 @@ import Home from '../Pages/Home';
 const AppRoutes: React.FC = () => {
   return (
     <Switch>
-        <Route exact path="/" component={Home}/>
+        <Route exact path="/home" component={Home}/>
     </Switch>
   );
 }
