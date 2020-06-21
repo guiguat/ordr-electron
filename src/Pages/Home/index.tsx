4bebfa6f5c58823e2327fdd8f29ddc1858182639
guiguat <guaturadzn@gmail.com>
2020-06-20 23:49:55 -0300

    style home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import app from '../../firebase';
 import Navbar from "../../Components/NavBar";
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div className="container-fluid h-100 w-100 p-0">
       <Navbar/>
-      <h1>Home</h1>
-      <button onClick={()=> app.auth().signOut()}>Sign Out</button>
-    </>  
+      <div className="container-fluid text-center pt-5 align-items-center justify-content-center py-auto">
+          <h1 className="logo-lg mb-3 mt-5">ORDR.</h1>
+          <p>The management system made with love for you restaurant</p>
+          <span>Support me on 
+            <a href="https://github.com/guiguat/ordr-offline-desktop" target="blank"> Github</a>
+          </span>
+      </div>
+    </div>  
   );
 }
 
