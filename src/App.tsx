3ad1e4fb62eda9155d623ebc627151c058976460
guiguat <guaturadzn@gmail.com>
2020-06-20 23:49:27 -0300

    import css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Routes from './routes/routes';
 import { AuthProvider } from "./Contexts/Auth";
+
+import "./custom.scss";
 function App() {
   return (
     <AuthProvider>
-      <Routes/>
+      <Routes />
     </AuthProvider>
   )
 }
