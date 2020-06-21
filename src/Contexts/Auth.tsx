0e1a52bea0a28f5e228629f41989f47c6dde5484
guiguat <guaturadzn@gmail.com>
2020-06-21 01:18:43 -0300

    add update delete

--- src/Contexts/Auth.tsx
+++ src/Contexts/Auth.tsx
@@ -1,16 +1,26 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import { User } from 'firebase';
 import app from "../firebase";
+import { RSA_NO_PADDING } from 'constants';
 
 interface IAuthContext {
     currentUser?: User | null;
     LogOut():void;
-    SignUp(signUpData: ISignUpData):void
+    SignUp(signUpData: ISignUpData):void;
+    DeleteUser(email:string, password:string):void;
+    UpdateUser(data:IUpdateData):void
 }
 interface ISignUpData{
     email:string;
     password:string;
     name:string;
+}
+interface IUpdateData{
+    email:string;
+    password:string;
+    newName?:string;
+    newEmail?:string;
+    newPassword?:string;
 }
 
 export const AuthContext = React.createContext<IAuthContext>({} as IAuthContext);
@@ -32,14 +42,41 @@ export const AuthProvider: React.FC = ({ children }) => {
             return result.user?.updateProfile({
                     displayName: signUpData.name
                 });
-            }).catch(function(error) {
-            console.log(error);
-            });
+            }).then(
+                ()=>alert("User Created")
+                ).catch(function(error) {
+                    alert(error);
+                    });
+    }
+
+    function DeleteUser(email:string, password:string){
+        app.auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(res=>{
+                res.user?.delete();
+                alert("User deleted")
+            })
+            .catch(error=>alert(error))
+    }
+
+    function UpdateUser(data:IUpdateData){
+        app
+            .auth()
+            .signInWithEmailAndPassword(data.email, data.password)
+            .then(res => {
+                if(data.newEmail) res.user?.updateEmail(data.newEmail);
+                if(data.newName) res.user?.updateProfile({displayName:data.newName});
+                if(data.newPassword) res.user?.updatePassword(data.newPassword);
+            })
+            .then(()=>alert("User data changed successfully"))
+            .catch(error => alert(error))
     }
 
     return (
-        <AuthContext.Provider value={ { currentUser, LogOut, SignUp } }>
+        <AuthContext.Provider value={ { currentUser, LogOut, SignUp, DeleteUser, UpdateUser } }>
             {children}
         </AuthContext.Provider>
     );
 }
+
+export const useAuth = () => useContext(AuthContext);
