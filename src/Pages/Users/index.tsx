bc1fa6438f052adf2f1737327029ba18788842d2
guiguat <guaturadzn@gmail.com>
2020-06-20 23:50:31 -0300

    create users page

---
+++ src/Pages/Users/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../Components/NavBar';
+// import { Container } from './styles';
+
+const Users: React.FC = () => {
+  return (
+      <>
+        <Navbar/>
+        <h1>Users</h1>
+      </>
+  );
+}
+
+export default Users;
