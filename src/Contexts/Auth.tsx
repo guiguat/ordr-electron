295a80c70ce122eed17eeba6a206d9389cbe1773
guiguat <guaturadzn@gmail.com>
2020-06-23 16:22:59 -0300

    add login function

--- src/Contexts/Auth.tsx
+++ src/Contexts/Auth.tsx
@@ -7,7 +7,8 @@ interface IAuthContext {
     LogOut():void;
     SignUp(signUpData: ISignUpData):void;
     DeleteUser(email:string, password:string):void;
-    UpdateUser(data:IUpdateData):void
+    UpdateUser(data:IUpdateData):void;
+    LogIn(email:string, password:string):void;
 }
 interface ISignUpData{
     email:string;
@@ -28,51 +29,63 @@ export const AuthProvider: React.FC = ({ children }) => {
     const [currentUser, setCurrentUser] = useState<User| null>(null);
 
     useEffect(()=>{
-        app.auth().onAuthStateChanged(userState => setCurrentUser(userState));        
+        app
+          .auth()
+          .onAuthStateChanged(userState => setCurrentUser(userState));        
     },[]);
 
     function LogOut(){
-        app.auth().signOut();
+        app
+          .auth()
+          .signOut();
+    }
+
+    function LogIn(email:string, password:string){
+        app
+        .auth()
+        .signInWithEmailAndPassword(email, password)
+        .catch(error=>alert(error));
     }
 
     function SignUp(signUpData:ISignUpData){
-        app.auth().createUserWithEmailAndPassword(signUpData.email, signUpData.password)
-            .then(result => {
+        app
+          .auth()
+          .createUserWithEmailAndPassword(signUpData.email, signUpData.password)
+          .then(result => {
             return result.user?.updateProfile({
-                    displayName: signUpData.name
-                });
-            }).then(
-                ()=>alert("User Created")
-                ).catch(function(error) {
-                    alert(error);
-                    });
+                displayName: signUpData.name
+            });
+          })
+          .then( ()=>alert("User Created"))
+          .catch(error=>alert(error));
     }
 
     function DeleteUser(email:string, password:string){
-        app.auth()
-            .signInWithEmailAndPassword(email, password)
-            .then(res=>{
-                res.user?.delete();
-                alert("User deleted")
-            })
-            .catch(error=>alert(error))
+        app
+          .auth()
+          .signInWithEmailAndPassword(email, password)
+          .then(res=>{
+            res.user?.delete();
+            alert("User deleted")
+          })
+          .catch(error=>alert(error))
     }
 
     function UpdateUser(data:IUpdateData){
         app
-            .auth()
-            .signInWithEmailAndPassword(data.email, data.password)
-            .then(res => {
-                if(data.newEmail) res.user?.updateEmail(data.newEmail);
-                if(data.newName) res.user?.updateProfile({displayName:data.newName});
-                if(data.newPassword) res.user?.updatePassword(data.newPassword);
-            })
-            .then(()=>alert("User data changed successfully"))
-            .catch(error => alert(error))
+          .auth()
+          .signInWithEmailAndPassword(data.email, data.password)
+          .then(res => {
+            if(data.newEmail) res.user?.updateEmail(data.newEmail);
+            if(data.newName) res.user?.updateProfile({displayName:data.newName});
+            if(data.newPassword) res.user?.updatePassword(data.newPassword);
+          })
+          .then(()=>alert("User data changed successfully"))
+          .catch(error => alert(error))
     }
 
     return (
-        <AuthContext.Provider value={ { currentUser, LogOut, SignUp, DeleteUser, UpdateUser } }>
+        <AuthContext.Provider value={ { currentUser, LogOut, SignUp, DeleteUser, UpdateUser, LogIn } }>
             {children}
         </AuthContext.Provider>
     );
