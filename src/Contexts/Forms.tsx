e7581d5bc9d11de6aa65c7fff06c06c25666976c
guiguat <guaturadzn@gmail.com>
2020-06-24 19:11:12 -0300

    add product state

--- src/Contexts/Forms.tsx
+++ src/Contexts/Forms.tsx
@@ -2,18 +2,28 @@ import React, {useState, useContext} from 'react';
 
 interface IFormsContext {
     btnClicked: string;
-    setBtnClicked(btnType:string):void
+    setBtnClicked(btnType:string):void;
+    prodSelected:IProductsData;
+    setProdSelected(product:IProductsData):void
 }
 
+interface IProductsData{
+    id: number,
+    name: string,
+    price: number,
+    stock: number,
+    type: string
+}
 
 export const FormsContext = React.createContext<IFormsContext>({} as IFormsContext);
 
 export const FormsProvider: React.FC = ({ children }) => {
 
     const [btnClicked, setBtnClicked] = useState("");
+    const [prodSelected, setProdSelected] = useState({} as IProductsData);
 
     return (
-        <FormsContext.Provider value={ { btnClicked, setBtnClicked } }>
+        <FormsContext.Provider value={ { btnClicked, setBtnClicked, prodSelected, setProdSelected } }>
             {children}
         </FormsContext.Provider>
     );
