9c2be0325a4d1c33896b93711f9112b6ca920bb5
Guilherme Guatura <guaturadzn@gmail.com>
2020-06-29 00:29:56 -0300

    change padding

--- src/Components/Col.tsx
+++ src/Components/Col.tsx
@@ -6,7 +6,7 @@ interface IColProps{
 }
 const Col: React.FC<IColProps> = ({children, type}) => {
     if(type === "nav") return <div className="col col-md-2 h-100 p-0">{children}</div>;
-    else return <div className="col col-md-10 h-100">{children}</div>
+    else return <div className="col col-md-10 px-4 py-3 h-100">{children}</div>
 }
 
 export default Col;
