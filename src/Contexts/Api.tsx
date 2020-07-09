ae8a603556de5eae3faaf7f59fc27bf7fa61a986
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-09 03:33:59 -0300

    add useSWR to context

--- src/Contexts/Api.tsx
+++ src/Contexts/Api.tsx
@@ -1,12 +1,16 @@
 import React, {useState, useContext} from 'react';
-
+import useSWR from 'swr';
 import Axios, { AxiosInstance } from "axios";
 
 interface IApiContext {
     Api: AxiosInstance;
-    setBaseURL(url:string):void
+    setBaseURL(url:string):void,
+    useAxios<Data=any, Error=any>(url:string): IuseAxios<Data, Error>;
 }
-
+interface IuseAxios<Data, Error>{
+    data: Data | undefined;
+    error: Error | undefined
+}
 
 export const ApiContext = React.createContext<IApiContext>({} as IApiContext);
 
@@ -18,8 +22,18 @@ export const ApiProvider: React.FC = ({ children }) => {
         baseURL
     })
 
+    function useAxios<Data=any, Error=any>(url:string){
+
+        const { data, error } = useSWR<Data, Error>(url, async url => {
+            const response = await Api.get(url);
+            return response.data;
+        })
+
+        return { data, error }
+    }
+
     return (
-        <ApiContext.Provider value={ { Api, setBaseURL } }>
+        <ApiContext.Provider value={ { Api, setBaseURL, useAxios } }>
             {children}
         </ApiContext.Provider>
     );
