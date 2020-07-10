0e470f6f1df4dabcd7672b3a7113b142aef9ac9e
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-10 04:09:32 -0300

    remove props and user page

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -3,55 +3,30 @@ import React from 'react';
 import './NavBar.scss';
 import { FiSettings } from 'react-icons/fi';
 
-interface Iprops {
-    primary:string;
-}
-
-const NavBar: React.FC<Iprops> = (props) => {
+const NavBar: React.FC = () => {
 
     return (
     
         <nav className="navbar navbar-dark px-0 py-3 bg-primary w-100 h-100">
             <span className="navbar-brand logo-lg mb-0 h1">ORDR.</span>
             <ul className="navbar-nav">
-                <li className={`nav-item ${props.primary === 'home'?'active':''}`}>
+                <li className="nav-item">
                     <a className="nav-link" href="#/">Home</a>
                 </li>
-                <li className={`nav-item ${props.primary === 'product'?'active':''}`}>
+                <li className="nav-item">
                     <a className="nav-link" href="#/product">Product</a>
                 </li>
-                <li className={`nav-item ${props.primary === 'report'?'active':''}`}>
+                <li className="nav-item">
                     <a className="nav-link" href="#/report">Report</a>
                 </li>
-                <li className={`nav-item ${props.primary === 'costumer'?'active':''}`}>
+                <li className="nav-item">
                     <a className="nav-link" href="#/costumer">Costumer</a>
-                </li>
-                <li className={`nav-item ${props.primary === 'user'?'active':''}`}>
-                    <a className="nav-link" href="#/user">User</a>
                 </li>
             </ul>
 
-            <a href="#/config" className="text-light nav-link">
-                        <FiSettings size={18} className="mr-2"/> Settings
-                    </a>
-            {/* <div className="dropdown">
-                <button className="btn btn-sm text-white dropdown-toggle"
-                 type="button" id="dropdownMenuButton"
-                 data-toggle="dropdown"
-                 aria-haspopup="true"
-                 aria-expanded="false"
-                >
-                    Hello, {currentUser?.displayName}
-                </button>
-                <div className="dropdown-menu text-right" aria-labelledby="dropdownMenuButton">
-                    <button onClick={LogOut} className="align-items-center d-flex font-weight-bold text-dark dropdown-item">
-                        <FiLogOut size={18} className="mr-2"/> Logout
-                    </button>
-                    <a href="#/config" className="align-items-center font-weight-bold d-flex text-dark dropdown-item">
-                        <FiSettings size={18} className="mr-2"/> Settings
-                    </a>
-                </div>
-            </div> */}
+            <a href="#/config" className="text-light nav-link nav-item">
+                <FiSettings size={18} className="mr-2"/> Settings
+            </a>
         </nav>
   
     );
