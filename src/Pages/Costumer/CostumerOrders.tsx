53854e44e75261d445ddb94e337d70ab32a034dc
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-10 06:05:56 -0300

    fix bugs and finish purchase

--- src/Pages/Costumer/CostumerOrders.tsx
+++ src/Pages/Costumer/CostumerOrders.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiInbox } from 'react-icons/fi';
 import { useApi } from '../../Contexts/Api';
 
 interface ICostumerOrdersProps{
@@ -6,41 +7,57 @@ interface ICostumerOrdersProps{
 }
 
 interface IProduct{
-    name:string,
+    order:string,
     price:number
 }
 
 const CostumerOrders: React.FC<ICostumerOrdersProps> = ({costumer_id}) => {
-    const { useAxios } = useApi();
+    const { useAxios, Api } = useApi();
     const { data, error } = useAxios<IProduct[]>(`/account?costumer_id=${costumer_id}`)
 
     if(error) console.log(error);
     if (!data) return <p>Loading...</p>
 
+    if(data.length === 0) return <p className="text-grey-800 mt-3 font-weight-bold ">No orders for now!</p>
+
+    async function handleFinishPurchase(){
+        try{
+            const res = await Api.delete(`/account?costumer_id=${costumer_id}`)
+            alert(res.data.message);
+        }
+        catch(error){
+            alert("An error occurred when trying to reach the server: \n"+ error);
+        }
+    }
+
     return (
     
-        <table className="table">
-            <caption>List of orders</caption>
-            <thead>
-                <tr>
-                    <th scope="col">Product</th>
-                    <th scope="col">Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    data?.map(
-                        (product:IProduct, index)=>(
-                            <tr key={index}>
-                                <th scope="row">{product.name}</th>
-                                <td>{product.price}</td>
-                            </tr>
+        <div className="container">
+            <table className="table">
+                <caption>List of orders</caption>
+                <thead>
+                    <tr>
+                        <th scope="col">Product</th>
+                        <th scope="col">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data?.map(
+                            (product:IProduct, index)=>(
+                                <tr key={index}>
+                                    <td>{product.order}</td>
+                                    <td>{`R$${product.price.toFixed(2)}`}</td>
+                                </tr>
+                            )
                         )
-                    )
-                }
-            </tbody>
-        </table>
-
+                    }
+                </tbody>
+            </table>
+            <button className="btn btn-dark" onClick={handleFinishPurchase}>
+                <FiInbox size={18} className="mr-2"/> Finish purchase
+            </button>
+        </div>
     );
 }
 
