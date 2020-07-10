a15c4601bf1b7ff873cc75a14bbcdc2c9ad65829
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-10 01:26:33 -0300

    add className props

--- src/Components/Col.tsx
+++ src/Components/Col.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-// import { Container } from './styles';
 interface IColProps{
-    type?:string
+    type?:string;
+    className?:string;
 }
-const Col: React.FC<IColProps> = ({children, type}) => {
-    if(type === "nav") return <div className="col col-md-2 h-100 p-0">{children}</div>;
-    else return <div className="col col-md-10 px-4 py-3 h-100 overflow-auto">{children}</div>
+const Col: React.FC<IColProps> = ({children, type, className}) => {
+    if(type === "nav") return <div className={`col col-md-2 h-100 p-0 ${className}`} >{children}</div>;
+    else return <div className={`col col-md-10 px-4 py-3 h-100 overflow-auto ${className}`}>{children}</div>
 }
 
 export default Col;
