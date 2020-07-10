4d615e7b9cab3948f87c97c57f7bc3a4bd74d01c
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-10 00:45:31 -0300

    add global navbar

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -11,51 +11,61 @@ import User from '../Pages/User';
 import Product from '../Pages/Product';
 import Report from '../Pages/Report';
 import Costumer from '../Pages/Costumer';
+import Col from '../Components/Col';
+import Container from '../Components/Container';
+import NavBar from '../Components/NavBar';
 
 const Routes: React.FC = () =>{
     const { currentUser } = useAuth();
 
     return ( 
         <Router>
-            <Switch>
-                <PrivateRoute 
-                isAuth={!!currentUser} 
-                path="/" 
-                redirectPath="/login"
-                exact 
-                component={Home}
-                />
+            <Container>
+                <Col type="nav">
+                    <NavBar primary="report"/>
+                </Col>
+                <Col>
+                    <Switch>
+                        <PrivateRoute 
+                        isAuth={!!currentUser} 
+                        path="/" 
+                        redirectPath="/login"
+                        exact 
+                        component={Home}
+                        />
 
-                <PrivateRoute
-                 isAuth={!!currentUser}
-                 path="/user"
-                 redirectPath="/login"
-                 component={User}
-                />
-                
-                <PrivateRoute
-                 isAuth={!!currentUser}
-                 path="/product"
-                 redirectPath="/login"
-                 component={Product}
-                />
+                        <PrivateRoute
+                        isAuth={!!currentUser}
+                        path="/user"
+                        redirectPath="/login"
+                        component={User}
+                        />
+                        
+                        <PrivateRoute
+                        isAuth={!!currentUser}
+                        path="/product"
+                        redirectPath="/login"
+                        component={Product}
+                        />
 
-                <PrivateRoute
-                 isAuth={!!currentUser}
-                 path="/report"
-                 redirectPath="/login"
-                 component={Report}
-                />
+                        <PrivateRoute
+                        isAuth={!!currentUser}
+                        path="/report"
+                        redirectPath="/login"
+                        component={Report}
+                        />
 
-                <PrivateRoute
-                 isAuth={!!currentUser}
-                 path="/costumer"
-                 redirectPath="/login"
-                 component={Costumer}
-                />
+                        <PrivateRoute
+                        isAuth={!!currentUser}
+                        path="/costumer"
+                        redirectPath="/login"
+                        component={Costumer}
+                        />
 
-                <Route path="/login" component={Login}/>
-            </Switch>
+                        <Route path="/login" component={Login}/>
+                    </Switch>
+                </Col>
+            </Container>
         </Router>
     );
 }
