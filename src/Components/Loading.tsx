8ee8b8983d774b78c069ac3b7cb5eb2242295608
Guilherme Guatura <guaturadzn@gmail.com>
2020-07-26 22:41:17 -0300

    center the spinner in the screen

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Loading: React.FC = () => {
   return (
-    <div className="spinner-border text-primary" role="status">
-        <span className="sr-only">Loading...</span>
+    <div className="d-flex align-items-center justify-content-center h-100">
+      <div className="spinner-border text-primary" role="status">
+          <span className="sr-only">Loading...</span>
+      </div>
     </div>
   );
 }
