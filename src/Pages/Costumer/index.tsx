9e1c10bc34e55c74820ea9709e9576a7d91d1b4f
guiguat <guaturadzn@gmail.com>
2020-07-28 23:35:51 -0300

    show create new costumer modal

--- src/Pages/Costumer/index.tsx
+++ src/Pages/Costumer/index.tsx
@@ -1,193 +1,241 @@
-import React, { useState } from 'react';
-import { useApi } from '../../Contexts/Api';
-import {FiUserPlus, FiTrash2, FiEdit} from 'react-icons/fi'
-import CostumerOrders from './CostumerOrders';
-import { ICostumer } from './models';
-import Loading from '../../Components/Loading';
+import React, { useState } from "react";
+import { useApi } from "../../Contexts/Api";
+import { FiUserPlus, FiTrash2, FiEdit } from "react-icons/fi";
+import CostumerOrders from "./CostumerOrders";
+import { ICostumer } from "./models";
+import Loading from "../../Components/Loading";
+import Modal from "react-bootstrap/Modal";
 
 const Costumer: React.FC = () => {
-    
-    const {Api, useAxios} = useApi();
-    const [costumer, setCostumer] = useState<ICostumer>({} as ICostumer);
-    const [name, setName] = useState("");
-    const [document, setDocument] = useState("");
+  const { Api, useAxios } = useApi();
+  const [costumer, setCostumer] = useState<ICostumer>({} as ICostumer);
+  const [name, setName] = useState("");
+  const [document, setDocument] = useState("");
+  const [showNewCostumer, setShowNewCostumer] = useState(false);
 
-    const {data, error} = useAxios<ICostumer[]>("/costumer");
+  const { data, error } = useAxios<ICostumer[]>("/costumer");
 
-    if(error) alert("An error occurred when trying to reach the server:\n"+error)
-    if(!data) return <Loading/>
+  if (error)
+    alert("An error occurred when trying to reach the server:\n" + error);
+  if (!data) return <Loading />;
 
-    async function createNewCostumer(event:any){
-        event.preventDefault();
-        try {
-            const data = {
-                name,
-                document
-            }
-            const response = await Api.post("/costumer", data);
-            alert(response.data.message);
-            setName("");
-            setDocument("");
-        } catch (e) {
-            alert("An error occurred when trying to reach the server:\n"+e);
-        }
+  async function createNewCostumer(event: any) {
+    event.preventDefault();
+    try {
+      const data = {
+        name,
+        document,
+      };
+      const response = await Api.post("/costumer", data);
+      alert(response.data.message);
+      setName("");
+      setDocument("");
+      setShowNewCostumer(false);
+    } catch (e) {
+      alert("An error occurred when trying to reach the server:\n" + e);
     }
+  }
 
-    async function updateCostumer(event:any){
-        event.preventDefault();
-        try {
-            if(costumer && costumer.id){
-                const data = {
-                    id:costumer.id,
-                    name,
-                    document
-                }
-                const response = await Api.put("/costumer", data);
-                alert(response.data.message);
-                setName("");
-                setDocument("");
-                setCostumer({} as ICostumer);
-            }
-        } catch (e) {
-            alert("An error occurred when trying to reach the server:\n"+e);
-        }
+  async function updateCostumer(event: any) {
+    event.preventDefault();
+    try {
+      if (costumer && costumer.id) {
+        const data = {
+          id: costumer.id,
+          name,
+          document,
+        };
+        const response = await Api.put("/costumer", data);
+        alert(response.data.message);
+        setName("");
+        setDocument("");
+        setCostumer({} as ICostumer);
+      }
+    } catch (e) {
+      alert("An error occurred when trying to reach the server:\n" + e);
     }
+  }
 
-    async function deleteCostumer(id:number){
-        try {
-            const response = await Api.delete(`/costumer?id=${id}`);
-            alert(response.data.message);
-        } catch (e) {
-            alert("An error occurred when trying to reach the server:\n"+e);
-        }
+  async function deleteCostumer(id: number) {
+    try {
+      const response = await Api.delete(`/costumer?id=${id}`);
+      alert(response.data.message);
+    } catch (e) {
+      alert("An error occurred when trying to reach the server:\n" + e);
     }
+  }
 
-    return (
-        <>
-            <h3 className="mb-4">Costumer</h3>
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#modalNewCostumer">
-                <FiUserPlus/>  Register New
+  return (
+    <>
+      <h3 className="mb-4">Costumer</h3>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={() => {
+          setShowNewCostumer(true);
+        }}
+      >
+        <FiUserPlus /> Register New
+      </button>
+
+      <Modal
+        show={showNewCostumer}
+        centered
+        onHide={() => {
+          setShowNewCostumer(false);
+        }}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>New Costumer</Modal.Title>
+        </Modal.Header>
+        <form onSubmit={createNewCostumer}>
+          <Modal.Body>
+            <div className="form-group">
+              <label htmlFor="name">Name *</label>
+              <input
+                type="text"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="form-control form-control-sm"
+                id="name"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="document">Document *</label>
+              <input
+                type="text"
+                required
+                value={document}
+                onChange={(e) => setDocument(e.target.value)}
+                className="form-control form-control-sm"
+                id="document"
+              />
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              className="btn btn-primary w-100"
+              disabled={!name || !document}
+              type="submit"
+            >
+              <FiUserPlus className="mb-1 mr-2" /> Create
             </button>
+          </Modal.Footer>
+        </form>
+      </Modal>
 
-            <div className="modal fade" id="modalNewCostumer" tabIndex={-1} role="dialog" aria-labelledby="modalNewCostumerLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="modalNewCostumerLabel">New Costumer</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <form onSubmit={createNewCostumer}>
-                            <div className="modal-body">
-                                <div className="form-group">
-                                    <label htmlFor="name">Name *</label>
-                                    <input type="text" required value={name}
-                                    onChange={e => setName(e.target.value)}
-                                    className="form-control form-control-sm" id="name"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="document">Document *</label>
-                                    <input type="text" required value={document}
-                                    onChange={e => setDocument(e.target.value)}
-                                    className="form-control form-control-sm" id="document"
-                                    />
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button 
-                                className="btn btn-primary w-100"
-                                disabled={ !name || !document }
-                                type="submit">
-                                    <FiUserPlus className="mb-1 mr-2"/> Create
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+      <div
+        className="modal fade"
+        id="editCostumerModal"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="editCostumerModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="editCostumerModalLabel">
+                Update info
+              </h5>
+              <button
+                type="button"
+                onClick={() => {
+                  setName("");
+                  setCostumer({} as ICostumer);
+                  setDocument("");
+                }}
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form onSubmit={updateCostumer}>
+              <div className="modal-body">
+                <div className="form-group">
+                  <label htmlFor="name">Name *</label>
+                  <input
+                    type="text"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control form-control-sm"
+                    id="name"
+                  />
                 </div>
-            </div>
-
-            <div className="modal fade" id="editCostumerModal" tabIndex={-1} role="dialog" aria-labelledby="editCostumerModalLabel" aria-hidden="true">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="editCostumerModalLabel">Update info</h5>
-                            <button 
-                            type="button" 
-                            onClick={()=>{setName(''); setCostumer({} as ICostumer); setDocument('')}}
-                            className="close" 
-                            data-dismiss="modal" 
-                            aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <form onSubmit={updateCostumer}>
-                            <div className="modal-body">
-                                <div className="form-group">
-                                    <label htmlFor="name">Name *</label>
-                                    <input type="text" required value={name}
-                                    onChange={e => setName(e.target.value)}
-                                    className="form-control form-control-sm" id="name"
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="document">Document *</label>
-                                    <input type="text" required value={document}
-                                    onChange={e => setDocument(e.target.value)}
-                                    className="form-control form-control-sm" id="document"
-                                    />
-                                </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button 
-                                disabled={ !name || !document }
-                                className="btn btn-warning w-100" 
-                                type="submit">
-                                    <FiEdit className="mb-1 mr-2"/> Update
-                                </button>
-                            </div>
-                        </form>
-                    </div>
+                <div className="form-group">
+                  <label htmlFor="document">Document *</label>
+                  <input
+                    type="text"
+                    required
+                    value={document}
+                    onChange={(e) => setDocument(e.target.value)}
+                    className="form-control form-control-sm"
+                    id="document"
+                  />
                 </div>
-            </div>
+              </div>
+              <div className="modal-footer">
+                <button
+                  disabled={!name || !document}
+                  className="btn btn-warning w-100"
+                  type="submit"
+                >
+                  <FiEdit className="mb-1 mr-2" /> Update
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
 
-            <div className="container-fluid mt-3 p-0" id="costumers">
-                {
-                    data?.map(costumer => {
-                        return (
-                            <div key={costumer.id} className="container-fluid mx-auto mb-4 bg-light rounded-lg p-4">
-                                <div className="row mb-3">
-                                    <div className="col col-md-11">
-                                        <h4 className="m-0">{costumer.name}</h4>
-                                        <span className="text-secondary">Document ID: {costumer.document}</span>
-                                    </div>
-                                    <div className="col col-md-1">
-                                        <div className="row p-0">
-                                            <button className="btn p-0 mr-1"
-                                             data-toggle="modal" data-target="#editCostumerModal"
-                                             onClick={e =>{
-                                                    setCostumer(costumer);
-                                                    setName(costumer.name);
-                                                    setDocument(costumer.document)
-                                                }
-                                            }>
-                                                <FiEdit className="text-warning" size={24}/>
-                                            </button>
-                                            <button className="btn p-0" onClick={()=>deleteCostumer(costumer.id)}>
-                                                <FiTrash2 className="text-danger" size={24}/>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <CostumerOrders costumer_id={costumer.id}/>
-                            </div>
-                        )
-                    })
-                }
+      <div className="container-fluid mt-3 p-0" id="costumers">
+        {data?.map((costumer) => {
+          return (
+            <div
+              key={costumer.id}
+              className="container-fluid mx-auto mb-4 bg-light rounded-lg p-4"
+            >
+              <div className="row mb-3">
+                <div className="col col-md-11">
+                  <h4 className="m-0">{costumer.name}</h4>
+                  <span className="text-secondary">
+                    Document ID: {costumer.document}
+                  </span>
+                </div>
+                <div className="col col-md-1">
+                  <div className="row p-0">
+                    <button
+                      className="btn p-0 mr-1"
+                      data-toggle="modal"
+                      data-target="#editCostumerModal"
+                      onClick={(e) => {
+                        setCostumer(costumer);
+                        setName(costumer.name);
+                        setDocument(costumer.document);
+                      }}
+                    >
+                      <FiEdit className="text-warning" size={24} />
+                    </button>
+                    <button
+                      className="btn p-0"
+                      onClick={() => deleteCostumer(costumer.id)}
+                    >
+                      <FiTrash2 className="text-danger" size={24} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <CostumerOrders costumer_id={costumer.id} />
             </div>
-        </>
-    );
-}
+          );
+        })}
+      </div>
+    </>
+  );
+};
 
 export default Costumer;
